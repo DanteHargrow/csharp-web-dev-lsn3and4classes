adf1caaa531729bbff136739726d9ed8621a657e
dantehargrow <dantehargrow@gmail.com>
2020-10-05 23:39:26 -0500

    added main program code

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,18 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            Student Dante = new Student();
+            Dante.NumberOfCredits = 1;
+            Dante.Gpa = 4.0;
+
+            Teacher Kim = new Teacher();
+            Course math = new Course();
+            Kim.FirstName = "Kim";
+            Kim.LastName = "Hargrow";
+            math.CourseName = "Math 101";
+            math.theTeacher(Kim.FirstName,Kim.LastName);
+            string teacherName = math.getTeacherName();
+            Console.WriteLine(teacherName);
         }
     }
 }
