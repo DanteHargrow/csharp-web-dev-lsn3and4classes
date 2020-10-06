6f52a9317c29949de750a17dd828b2bd33406707
dantehargrow <dantehargrow@gmail.com>
2020-10-05 23:42:27 -0500

    added new classes and their methods and properties

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPractice
+{
+    class Course
+    {
+        public string CourseName { get; set; }
+        List<string> students = new List<string> { "Dante", "Mike", "Tim" };
+        private string teacherName;
+
+        public void theTeacher(string firstName, string lastName)
+        {
+            this.teacherName = firstName+" "+lastName;
+        }
+        public string getTeacherName()
+        {
+            return $"The teacher of this course is {this.teacherName}";
+        }
+
+    }
+}
